272a1d8af055ca636161a3a083e6d69852afc623
TBlackmore <tim.r.blackmore@gmail.com>
2017-03-27 09:51:54 +1100

    Moved 'tryCatch' into function instead of function call. Allows finer control of parameters output format

--- OpenR_Logistic_Fit_for_dotmatics.R
+++ OpenR_Logistic_Fit_for_dotmatics.R
@@ -16,7 +16,7 @@ maxUpper = Inf
 maxInflection = Inf
 minInflection = 0
 slopeLimits = '-Inf,0'
-fitForEach = 'Sample'
+fitForEach = 'replicate'
 EDLevel = 50
 EDType
 
@@ -86,38 +86,40 @@ upperlimit <- c(if (fixedSlopeOn) NULL else maxSlope, #Slope
 #Function to perform fit and reformat parameters
 fit4pl <- function (df) {
   print(df$SAMPLE_ID[1])
-  fit <- drm(df$RESPONSE~df$CONC, 
-      data = sampleData, 
-      fct = LL.4(fixed=c(fixedSlopeVal, fixedLowerVal, fixedUpperVal, NA), 
-            names = c("Slope", "Lower Limit", "Upper Limit", "ed")), 
-      control = drmc(errorm = FALSE, warnVal = -1, noMessage = TRUE),
-      lowerl = lowerlimit,
-      upperl = upperlimit
-      )
-  parms <- c(fit$parmMat)
-  names(parms) <- unlist(fit$parNames[1])
-  #add fixed parameters to parms to keep number of columns consistent
-  if (fixedSlopeOn) parms <- append(parms, fixedSlopeVal,0)
-  if (fixedLowerOn) parms <- append(parms, fixedLowerVal,1)
-  if (fixedUpperOn) parms <- append(parms, fixedUpperVal,2)
-  ed <- ED(fit,EDLevel, interval = "delta", type = EDType)
-  edflat <- c(ed)
-  names(edflat) <- c(outer("ED", colnames(ed), paste, sep = ":"))
-  parms <- c(parms, edflat)
-  return(parms)
+  groups <- c(UNIQUE_PROP_ID = paste(unique(df$UNIQUE_PROP_ID), collapse = ", "))
+  groups <- c(groups, SAMPLE_PLATE_ID = paste(unique(df$SAMPLE_PLATE_ID), collapse = ", "))
+  tryCatch({
+    fit <- drm(df$RESPONSE~df$CONC, 
+               data = sampleData, 
+               fct = LL.4(fixed=c(fixedSlopeVal, fixedLowerVal, fixedUpperVal, NA), 
+                          names = c("Slope", "Lower Limit", "Upper Limit", "ed")), 
+               control = drmc(errorm = FALSE, warnVal = -1, noMessage = TRUE),
+               lowerl = lowerlimit,
+               upperl = upperlimit
+              )
+    parms <- c(fit$parmMat)
+    if (length(parms) > 0) {
+      names(parms) <- unlist(fit$parNames[1])
+      if (fixedSlopeOn) parms <- append(parms, fixedSlopeVal,0)
+      if (fixedLowerOn) parms <- append(parms, fixedLowerVal,1)
+      if (fixedUpperOn) parms <- append(parms, fixedUpperVal,2)
+      ed <- ED(fit,EDLevel, interval = "delta", type = EDType)
+      edflat <- c(ed)
+      names(edflat) <- c(outer("ED", colnames(ed), paste, sep = ":"))
+      parms <- c(parms, edflat)
+    } else {
+      parms <- rep(NaN, 8)
+    }
+    
+  })
+  return(c(groups,parms))
 }
 
-
 # Group data by SAMPLE_ID and apply fit function to each group
-out <- ddply(sampleData, groupBy,
-             function(t) tryCatch(fit4pl(t), 
-                                  error=function(x) {
-                                    rep(NaN,8)
-                                  })
-)
+out <- ddply(sampleData, groupBy, function(t) fit4pl(t))
 
 
-names(out) <- c("SAMPLE_ID", "EXPERIMENT_ID", "PROTOCOL_ID", "Slope", "Lower Limit", "Upper Limit", "Inflection Point", "ED Estimate", "ED Std Error", "ED Lower CI", "ED Upper CI")
+names(out) <- c("SAMPLE_ID", "EXPERIMENT_ID", "PROTOCOL_ID", "UNIQUE_PROP_ID", "SAMPLE_PLATE_ID", "Slope", "Lower Limit", "Upper Limit", "Inflection Point", "ED Estimate", "ED Std Error", "ED Lower CI", "ED Upper CI")
 out <- cbind(out,
             "fixedLower" = fixedLower,
             "fixedUpper" = fixedUpper,
@@ -134,7 +136,7 @@ out <- cbind(out,
             "EDType" = EDType)
 #Replace NaN with NA
 out[is.na(out)] <- NA            
-
+as.numeric(out)
 #Function to round all numeric columns in a data frame
 round_df <- function(df, digits) {
   nums <- vapply(df, is.numeric, FUN.VALUE = logical(1))
@@ -143,63 +145,4 @@ round_df <- function(df, digits) {
   
   (df)
 }
-
 out <- round_df(out,30)
-
-
-#tf <- daply(out, ~SAMPLE_ID, function(x) nchar(x$SAMPLE_ID)) > 10
-#out[c(which(tf)),]
-
-help(ddply)
-fit4pl(tmp)
-
-### test with generated data
-# variables
-n <- 22 # number of data points
-t <- 10^(seq(log10(0.1),log10(100),,22))
-min <- 0
-max <- 100
-hill <- -.7
-X50 <- 20
-c.unif <- runif(n)
-c.norm <- rnorm(n) *2
-
-
-# generate data and calculate "y"
-set.seed(1)
-y1 <- min + ((max - min) / (1 + 10^(hill*(log10(X50)- log10(t)))))  + c.norm # uniform error
-
-fit <- drm(y1~t, 
-           fct = LL.4(fixed=c(NA, NA, NA, NA), 
-          names = c("Slope", "Lower Limit", "Upper Limit", "ed")), 
-           control = drmc(errorm = FALSE, warnVal = -1, noMessage = TRUE))
-ED(fit,50)
-plot(fit)
-
-tmp <- subset.data.frame(Input, Input$SAMPLE_ID == "WEHI-1213557-001")
-fit <- drm(tmp$RESPONSE~tmp$CONC, 
-           fct = LL.5(fixed=c(NA, 1, NA, NA, 1), 
-                      names = c("Slope", "Lower Limit", "Upper Limit", "ed", "skew")), 
-           control = drmc(errorm = TRUE, warnVal = 1, noMessage = FALSE))
-
-lowerlimit <- c(-Inf,  -Inf, 0)
-upperlimit <- c(Inf,  maxUpper, maxInflection)
-
-fit <- drm(RESPONSE~CONC, 
-           data = tmp, 
-           fct = LL.5(fixed=c(NA, 0, NA, NA, 1), 
-                      names = c("Slope", "Lower Limit", "Upper Limit", "ed", "Skew")), 
-           control = drmc(errorm = FALSE, warnVal = -1, noMessage = TRUE),
-           lowerl = lowerlimit,
-           upperl = upperlimit
-)
-fit
-
-out <- ddply(sampleData, "SAMPLE_ID", fit4pl)
-out <- dlply(sampleData,"SAMPLE_ID",
-               function(t) tryCatch(fit4pl(t), 
-                                    error=function(x) {
-                                      cat("error occurred for:\n", 
-                                          "\n...skipping this ticker\n")
-                                    })
-)
